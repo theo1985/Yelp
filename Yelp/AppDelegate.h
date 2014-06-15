1390f8ee3f559d0ef2a1644650a8e230be493352
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2014-06-15 16:04:00 -0700

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Theofanis Pantelides on 6/15/14.
+//  Copyright (c) 2014 theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yelp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Theofanis Pantelides on 6/15/14.
+//  Copyright (c) 2014 theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
